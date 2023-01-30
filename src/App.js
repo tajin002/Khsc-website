5efc7e36986a1374f512ddf8f72607496e405785
tajin002 <tajinshawon002@gmail.com>
2023-01-30 23:19:40 +0600

    just apply

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     },6000)
   },[])
   return (
-    <div className='max-w-[1440px] mx-auto bg-blue-300'>
+    <div className='max-w-[1440px] mx-auto'>
       {
         loading ? 
         <div className='flex justify-center mt-60'>
@@ -21,6 +21,7 @@ function App() {
           {/* <p className='text-blue-400 text-2xl font-bold mb-5 ml-4'>KHSC</p> */}
           <img className='h-1/2 ml-20' src={logo} alt="" />
           <progress className="progress w-56"></progress>
+          <h1 className='text-amber-300'>hello</h1>
           </div>
         </div>
         :
